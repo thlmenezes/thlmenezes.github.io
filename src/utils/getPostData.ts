c17983e046dd68f86898adc0de5f0ac0ab4bc73f
Thales Menezes <thlmenezes@pm.me>
2022-08-29 04:12:42 -0300

    📝 (utils): JSDoc code

--- src/utils/getPostData.ts
+++ src/utils/getPostData.ts
@@ -1,3 +1,14 @@
+/**
+ * Extract slug from path/to/file string
+ * 
+ * @example
+ * // => 'saga-creche-continua'
+ * getSlugFromFile('blog/saga-creche-continua');
+ *
+ * @param {string} file - 'path/to/file'
+ * 
+ * @returns {string} file slug extract from path like string
+ */
 export function getSlugFromFile(file: string) {
   return file.split('/').pop()?.split('.').shift() ?? ""
 }
