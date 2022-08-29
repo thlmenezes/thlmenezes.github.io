5ad316183585dc27592f7c8067816e24d017a9d4
Thales Menezes <thlmenezes@pm.me>
2022-08-28 22:21:25 -0300

    🔧 (astro): inject reading time in md frontmatter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +17,7 @@ export default defineConfig({
           target: '_blank',
         },
       ],
+      remarkReadingTime
     ],
   },
 })
